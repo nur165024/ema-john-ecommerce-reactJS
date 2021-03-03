a95979382031c6bf2e9b02c8ebf16498888e496e
nur165024 <nur42658@gmail.com>
2021-03-03 14:44:49 +0600

    cart some update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,14 +2,27 @@ import React from 'react';
 
 const Cart = (props) => {
     const cart = props.cart;
-    const totalPrice = cart.reduce( (total,prod) => total + prod.price,0);
-    const numberRound = (data) => {
-        Math.round(data);
+    // const totalPrice = cart.reduce( (total,prod) => total + prod.price,0);
+    let totalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+        let productPrice = cart[i];
+        totalPrice = totalPrice + productPrice.price;
     }
+    let shippingCost = 0;
+    if(totalPrice > 35){
+        shippingCost = 0;
+    }else if(totalPrice > 12){
+        shippingCost = 4.99;
+    }else if(totalPrice > 0){
+        shippingCost = 12.99;
+    }
+    
     return (
         <div>
             <h2>Cart list</h2>
-            <h5>Item Ordered : {cart.length}</h5>
+            <p>Items Ordered : {cart.length}</p>
+            <p><small>Shipping Cost : {shippingCost}</small></p>
+            <p>Total Price : $ {(totalPrice + shippingCost).toFixed(2)}</p>
         </div>
     );
 };
